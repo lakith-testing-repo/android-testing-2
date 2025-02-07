6462f5314647758ccf70b81bff1cf1c7f22e7aae
Lakith Rathnayake <lakithpathmila@gmail.com>
2025-02-07 15:48:21 +0530

    Add showSelectedLocationAndCreateGeofence()

--- app/src/main/java/com/lakithrathnayake/myapplication02/MapsActivity.java
+++ app/src/main/java/com/lakithrathnayake/myapplication02/MapsActivity.java
@@ -5,7 +5,11 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import com.google.android.gms.location.GeofencingClient;
 import com.google.android.gms.location.LocationServices;
@@ -13,15 +17,21 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.lakithrathnayake.myapplication02.databinding.ActivityMapsBinding;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
     private GeofencingClient geofencingClient;
+    private ExecutorService executorService;
+    private Handler mainHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +40,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        executorService = Executors.newFixedThreadPool(3);
+        mainHandler = new Handler(Looper.getMainLooper());
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         geofencingClient = LocationServices.getGeofencingClient(this);
+
+        LatLng selectedLocation = getSelectedLocation();
+        showSelectedLocationAndCreateGeofence(selectedLocation);
+
     }
 
     /**
@@ -61,5 +78,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
         }
+    }
+
+    private LatLng getSelectedLocation() {
+        return new LatLng(7.145652594871874, 80.56871574361789);
+    }
+
+    private void showSelectedLocationAndCreateGeofence(LatLng selectedLatLng) {
+        executorService.execute(() -> {
+            MarkerOptions markerOptions = new MarkerOptions().position(selectedLatLng);
+            CircleOptions circleOptions = new CircleOptions()
+                    .center(selectedLatLng)
+                    .radius(80)
+                    .strokeColor(Color.argb(255, 0, 0, 255))
+                    .fillColor(Color.argb(64, 0, 0, 255))
+                    .strokeWidth(4);
+
+            mainHandler.post(() -> {
+                mMap.addMarker(markerOptions);
+                mMap.addCircle(circleOptions);
+
+            });
+        });
     }
 }
